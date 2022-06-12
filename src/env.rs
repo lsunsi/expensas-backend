bda9fe04ae3c51cb7e6da5092524462fca58fcd2
Lucas Sunsi Abreu <lsunsi@pm.me>
2022-06-12 12:30:23 -0300

    Add basic server structure

---
+++ src/env.rs
@@ -0,0 +1,36 @@
+use anyhow::Context;
+use std::{collections::HashMap, error::Error, net::SocketAddr, str::FromStr};
+
+pub struct Env {
+    pub rest_socket: SocketAddr,
+    pub database_url: String,
+}
+
+pub async fn init() -> anyhow::Result<Env> {
+    let file = tokio::fs::read_to_string(ENV_PATH)
+        .await
+        .context("No env file found")?;
+
+    let mut map = file
+        .split('\n')
+        .filter_map(|s| s.split_once('='))
+        .collect::<HashMap<&str, &str>>();
+
+    Ok(Env {
+        rest_socket: read(&mut map, "REST_SOCKET")?,
+        database_url: read(&mut map, "DATABASE_URL")?,
+    })
+}
+
+fn read<T, E>(map: &mut HashMap<&str, &str>, key: &str) -> anyhow::Result<T>
+where
+    T: FromStr<Err = E>,
+    E: Error + Send + Sync + 'static,
+{
+    map.get(key)
+        .with_context(|| format!("Key missing: {key}"))?
+        .parse()
+        .with_context(|| format!("Key unparsable: {key}"))
+}
+
+const ENV_PATH: &str = ".env";
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use axum::routing::get;
+use sqlx::postgres::PgPoolOptions;
+
+mod env;
+mod queries;
+mod routes;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let env = env::init().await?;
+
+    let pg = PgPoolOptions::new().connect(&env.database_url).await?;
+
+    let router = axum::Router::new().route("/", get(routes::oiblz));
+
+    axum::Server::bind(&env.rest_socket)
+        .serve(router.into_make_service())
+        .await?;
+
+    Ok(())
 }
