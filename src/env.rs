6c002fd971f3f26a24e04f8643afff151ac8935d
Lucas Sunsi Abreu <lsunsi@pm.me>
2022-07-31 16:39:15 -0300

    Remove requirement for env file on prod

--- src/env.rs
+++ src/env.rs
@@ -1,5 +1,5 @@
 use anyhow::Context;
-use std::{collections::HashMap, error::Error, net::SocketAddr, str::FromStr};
+use std::{error::Error, net::SocketAddr, str::FromStr};
 
 pub struct Env {
     pub rest_socket: SocketAddr,
@@ -7,15 +7,37 @@ pub struct Env {
     pub secret: String,
 }
 
+#[cfg(not(debug_assertions))]
 pub async fn init() -> anyhow::Result<Env> {
-    let file = tokio::fs::read_to_string(ENV_PATH)
+    Ok(Env {
+        rest_socket: read("REST_SOCKET")?,
+        database_url: read("DATABASE_URL")?,
+        secret: read("SECRET")?,
+    })
+}
+
+#[cfg(not(debug_assertions))]
+fn read<T, E>(key: &str) -> anyhow::Result<T>
+where
+    T: FromStr<Err = E>,
+    E: Error + Send + Sync + 'static,
+{
+    std::env::var(key)
+        .with_context(|| format!("Key missing: {key}"))?
+        .parse()
+        .with_context(|| format!("Key unparsable: {key}"))
+}
+
+#[cfg(debug_assertions)]
+pub async fn init() -> anyhow::Result<Env> {
+    let file = tokio::fs::read_to_string(".env")
         .await
         .context("No env file found")?;
 
     let mut map = file
         .split('\n')
         .filter_map(|s| s.split_once('='))
-        .collect::<HashMap<&str, &str>>();
+        .collect::<std::collections::HashMap<&str, &str>>();
 
     Ok(Env {
         rest_socket: read(&mut map, "REST_SOCKET")?,
@@ -24,7 +46,8 @@ pub async fn init() -> anyhow::Result<Env> {
     })
 }
 
-fn read<T, E>(map: &mut HashMap<&str, &str>, key: &str) -> anyhow::Result<T>
+#[cfg(debug_assertions)]
+fn read<T, E>(map: &mut std::collections::HashMap<&str, &str>, key: &str) -> anyhow::Result<T>
 where
     T: FromStr<Err = E>,
     E: Error + Send + Sync + 'static,
@@ -34,5 +57,3 @@ where
         .parse()
         .with_context(|| format!("Key unparsable: {key}"))
 }
-
-const ENV_PATH: &str = ".env";
